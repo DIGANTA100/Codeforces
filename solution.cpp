847f9dec4289097cae8527ed26eee1a4ac1e9c19
DIGANTA100 <ajmainfayekdiganta@gmail.com>
2026-03-13 07:43:51 +0600

    new problem

--- solution.cpp
+++ solution.cpp
@@ -1,59 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+void solve()
 {
     int n;
     cin >> n;
-    for (int i = 0; i <= n; i++)
+    queue<int> q;
+    bool f = false;
+    int cur = -1;
+    int curcnt = 0;
+    vector<int> v(n + 1);
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= n - i; j++)
-            cout << "  ";
-
-        for (int j = 0; j < 2 * i + 1; j++)
+        int x;
+        cin >> x;
+        v[i] = x;
+        if (x != cur)
         {
-            if (j <= i)
-            {
+            q.push(i);
+            cur = x;
+            if (curcnt == 1)
+                f = true;
 
-                if (j != 2 * i)
-                    cout << j << ' ';
-                else
-                    cout << j;
-            }
-            else
-            {
-                if (j != 2 * i)
-                    cout << 2 * i - j << ' ';
-                else
-                    cout << 2 * i - j;
-            }
+            curcnt = 0;
         }
 
-        cout << endl;
+        curcnt++;
     }
-    for (int i = n - 1; i >= 0; i--)
+
+    if (f or n==1 or curcnt==1)
     {
-        for (int j = 1; j <= n - i; j++)
-            cout << "  ";
-        for (int j = 0; j < 2 * i + 1; j++)
-        {
-            if (j <= i)
-            {
+        cout << -1;
+        return;
+    }
 
-                if (j != 2 * i)
-                    cout << j << ' ';
-                else
-                    cout << j;
-            }
-            else
-            {
-                if (j != 2 * i)
-                    cout << 2 * i - j << ' ';
-                else
-                    cout << 2 * i - j;
-            }
-        }
+    while (!q.empty())
+    {
+        int first = q.front();
 
+        int last;
+        q.pop();
+        if (!q.empty())
+            last = q.front();
+
+        else
+            last = v.size();
+
+        int sz = last - first;
+
+        cout << last - 1 << ' ';
+        for (int i = first; i < last - 1; i++)
+            cout << i << ' ';
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
         cout << endl;
     }
 }
