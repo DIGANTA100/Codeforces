6bb8a1aeb466b5b551d835a9eea160ba1178dd4b
DIGANTA100 <ajmainfayekdiganta@gmail.com>
2026-03-17 18:14:20 +0600

    new problem

--- 800To1000/A. Bad Ugly Numbers.cpp
+++ 800To1000/A. Bad Ugly Numbers.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 void solve()
 {
     int n;
@@ -10,39 +9,29 @@ void solve()
         cout << -1;
         return;
     }
-
     int sum = 3 + (n - 1) * 2;
     bool need4 = false;
-
     if (sum % 3 == 0)
         need4 = true;
-
     string s;
-
     for (int i = 0; i < n - 2; i++)
     {
         s += '2';
     }
     if (need4)
         s += '4';
-
     else
         s += '2';
-
     s += '3';
-
     cout << s;
 }
-
 int main()
 {
     int t;
-
     cin >> t;
-
     while (t--)
     {
         solve();
-        cout << endl;
+           cout << endl;
     }
 }
