286119725d1d24db5d152ffd9b524b323449e079
DIGANTA100 <ajmainfayekdiganta@gmail.com>
2026-03-18 20:27:11 +0600

    new problem

--- 800To1000/A. Payment Without Change.cpp
+++ 800To1000/A. Payment Without Change.cpp
@@ -1,54 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 void solve()
 {
     long long a, b, n, S;
     cin >> a >> b >> n >> S;
-    // if(n==1 and a+b>=S){
-    //     cout<<"YES";
-    //     return;
-    // }
-
-
     if (S / n <= a)
     {
-
-        long long rem = S % n;
-
-        if (rem <= b)
+      long long rem = S % n;
+     if (rem <= b)
         {
-
             cout << "YES";
             return;
         }
-
         else
         {
             cout << "NO";
             return;
         }
     }
-
     else if (S <= b+a*n)
     {
         cout << "YES";
         return;
     }
-
     else
     {
         cout << "NO";
         return;
     }
 }
-
 int main()
 {
     int t;
-
     cin >> t;
-
     while (t--)
     {
         solve();
