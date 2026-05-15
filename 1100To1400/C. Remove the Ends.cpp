ca6ab1966cc05a28db65cab7bbea83bde14e7acc
DIGANTA100 <ajmainfayekdiganta@gmail.com>
2026-05-15 22:03:45 +0600

    new problme

--- 1100To1400/C. Remove the Ends.cpp
+++ 1100To1400/C. Remove the Ends.cpp
@@ -1,12 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-#define debug(x) cout << #x << " = " << x << "\n";
-#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-int uid(int a, int b) { return uniform_int_distribution<int>(a, b)(rng); }
-ll uld(ll a, ll b) { return uniform_int_distribution<ll>(a, b)(rng); }
-
 void solve(){
     int n;
     cin >> n;
@@ -42,10 +36,7 @@ void solve(){
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    
+   
     int t;
     cin >> t;
     while (t--) solve();
